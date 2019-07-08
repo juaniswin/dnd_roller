ccd00dfbdca9fef91c8037839dab975ce6e2e0d1
deiros.jd@gmail.com <Jyuuken3#>
2019-07-08 13:38:42 -0400

    Let PyCharm clean up my code, I guess?

--- dnd_roller.py
+++ dnd_roller.py
@@ -1,36 +1,43 @@
-#4 side die
-print ('four side')
+# 4 side die
+print('four side')
 from random import *
-print (randint(1,4))
 
-#6 side die
-print ('six side')
-from random import *
-print (randint(1,6))
+print(randint(1, 4))
 
-#8 side die
-print ('eight side')
+# 6 side die
+print('six side')
 from random import *
-print (randint(1,8))
 
-#10 side die
-print ('ten side')
-from random import *
-print (randint(1,10))
+print(randint(1, 6))
 
-#12 side die
-print ('twelve side')
+# 8 side die
+print('eight side')
 from random import *
-print (randint(1,12))
 
-#20 side die
-print ('twenty side')
-from random import *
-print (randint(1,20))
+print(randint(1, 8))
 
-#10s interval die
-print ('tens to hundred')
+# 10 side die
+print('ten side')
 from random import *
-items = [10,20,30,40,50,60,70,80,90,100]
-x = sample(items,  1)
-print (x[0])
+
+print(randint(1, 10))
+
+# 12 side die
+print('twelve side')
+from random import *
+
+print(randint(1, 12))
+
+# 20 side die
+print('twenty side')
+from random import *
+
+print(randint(1, 20))
+
+# 10s interval die
+print('tens to hundred')
+from random import *
+
+items = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+x = sample(items, 1)
+print(x[0])
