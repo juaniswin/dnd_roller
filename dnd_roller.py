f9741262a5b9573f6b8934bc6edd0b2f73b4190b
deiros.jd@gmail.com <Jyuuken3#>
2019-07-08 13:51:19 -0400

    Cleaned up/Shortened Code; learned how Module works.

--- dnd_roller.py
+++ dnd_roller.py
@@ -1,43 +1,18 @@
-# 4 side die
-print('four side')
 from random import *
 
+print("~d4~")
 print(randint(1, 4))
-
-# 6 side die
-print('six side')
-from random import *
-
+print("~d6~")
 print(randint(1, 6))
-
-# 8 side die
-print('eight side')
-from random import *
-
+print("~d8~")
 print(randint(1, 8))
-
-# 10 side die
-print('ten side')
-from random import *
-
+print("~d10~")
 print(randint(1, 10))
-
-# 12 side die
-print('twelve side')
-from random import *
-
+print("~d12~")
 print(randint(1, 12))
-
-# 20 side die
-print('twenty side')
-from random import *
-
+print("~d20~")
 print(randint(1, 20))
-
-# 10s interval die
-print('tens to hundred')
-from random import *
-
+print("~hundreds integer~")
 items = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 x = sample(items, 1)
 print(x[0])
