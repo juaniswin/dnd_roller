59e646a6ae1a3bdd0736eb17d4a3cfb22b476e46
deiros.jd@gmail.com <Jyuuken3#>
2019-07-08 23:56:56 -0400

    v1.1

--- dnd_if_elif.py
+++ dnd_if_elif.py
@@ -1,5 +1,7 @@
 from random import *
 
+print("Press d on the keyboard then fill in for (x); x = 4, 6, 8, 10, 12, 20, or 100")
+
 
 def d(dice):
     if dice == 4:
@@ -14,7 +16,7 @@ def d(dice):
         print(randint(1, 10))
     if dice == 20:
         print(randint(1, 12))
-    elif dice == 100:
+    if dice == 100:
         items = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
         x = sample(items, 1)
         print(x[0])
