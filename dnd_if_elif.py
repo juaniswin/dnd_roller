e2f78b8160dd86caf64742a333c381f893329eb8
deiros.jd@gmail.com <Jyuuken3#>
2019-07-08 23:25:08 -0400

    v1.1

---
+++ dnd_if_elif.py
@@ -0,0 +1,19 @@
+from random import *
+
+def dice(die):
+    if die == 4:
+        print(randint(1, 4))
+    if die == 6:
+        print(randint(1, 6))
+    if die == 8:
+        print(randint(1, 8))
+    if die == 10:
+        print(randint(1, 10))
+    if die == 12:
+        print(randint(1, 10))
+    if die == 20:
+        print(randint(1, 12))
+    elif die == 100:
+        items = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        x = sample(items, 1)
+        print(x[0])
